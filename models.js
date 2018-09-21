5b917a44aa3c749ee58ba2d01101ec25d548a292
Mike <mike.bryant.jr@icloud.com>
2018-09-20 20:31:27 -0600

    fixed database issue

--- models.js
+++ models.js
@@ -1,12 +1,3 @@
-// This module provides volatile storage, using a `BlogPost`
-// model. We haven't learned about databases yet, so for now
-// we're using in-memory storage. This means each time the app stops, our storage
-// gets erased.
-
-// Don't worry too much about how BlogPost is implemented.
-// Our concern in this example is with how the API layer
-// is implemented, and getting it to use an existing model.
-
 const mongoose = require("mongoose");
 
 const blogPostSchema = mongoose.Schema({
@@ -29,27 +20,10 @@ const blogPostSchema = mongoose.Schema({
 });
 
 
-// *virtuals* (http://mongoosejs.com/docs/guide.html#virtuals)
-// allow us to define properties on our object that manipulate
-// properties that are stored in the database. Here we use it
-// to generate a human readable string based on the address object
-// we're storing in Mongo.
 blogPostSchema.virtual("authorName").get(function () {
     return `${this.author.firstName} ${this.author.lastName}`.trim();
 });
 
-// this virtual grabs the most recent grade for a restaurant.
-// blogPostSchema.virtual("grade").get(function () {
-//     const gradeObj =
-//         this.grades.sort((a, b) => {
-//             return b.date - a.date;
-//         })[0] || {};
-//     return gradeObj.grade;
-// });
-
-// this is an *instance method* which will be available on all instances
-// of the model. This method will be used to return an object that only
-// exposes *some* of the fields we want from the underlying data
 blogPostSchema.methods.serialize = function () {
     return {
         id: this._id,
@@ -60,9 +34,8 @@ blogPostSchema.methods.serialize = function () {
     };
 };
 
-// note that all instance methods and virtual properties on our
-// schema must be defined *before* we make the call to `.model`.
-const BlogPost = mongoose.model("BlogPost", blogPostSchema);
+// tie the database in the first "" part of the method
+const BlogPost = mongoose.model("posts", blogPostSchema);
 
 module.exports = {
     BlogPost
